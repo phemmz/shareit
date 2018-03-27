1213f529d61ecf3f39dbb3ac7b7cea6fae119a5f
noordean <ebroyeem90@gmail.com>
2018-03-27 13:13:37 +0100

    initialize project

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server now running");
+});
